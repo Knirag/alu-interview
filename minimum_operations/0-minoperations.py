c119c9e7cdf46c12d5e8b0ede6772c6dabfd5866
Knirag <105249761+Knirag@users.noreply.github.com>
2023-02-10 23:45:46 +0200

    tammy

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,11 +4,11 @@
 def minOperations(p):
     k = 0
     b = 2
-
-while p > 1;
+"""Variables Declared"""
+while p > 1:
 while p % b == 0:
     k += b
     p /= b
     b +=1
-
+"""Arthimetic operations done"""
     return k
